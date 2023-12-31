5fcbfb1a05c8cea830e76da260427d23cd1c21e4
Sudharsan7303 <sudharsan7303@gmail.com>
2023-12-31 14:15:56 +0530

    changed db to postgresql

--- Todo_Website/settings.py
+++ Todo_Website/settings.py
@@ -78,8 +78,11 @@ WSGI_APPLICATION = 'Todo_Website.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'tododb',
+        'USER':'Sudharsan',
+        'PASSWORD': 'password',
+
     }
 }
 
