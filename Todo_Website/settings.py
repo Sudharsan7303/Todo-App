5f3c573572c49d41a79a9c66da340aa0ab77d481
Sudharsan7303 <sudharsan7303@gmail.com>
2024-07-19 21:12:50 +0530

    Removed Pyaudio for deploy

--- Todo_Website/settings.py
+++ Todo_Website/settings.py
@@ -32,7 +32,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-(i9(8hpcm_(=k$m!m)+^h-u90mwh4e#xcs86+zfp40b0@ztzkd'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
@@ -94,6 +94,7 @@ DATABASES = {
     }
 }
 '''
+
 
 # Database-Live Postgresql
 import dj_database_url
